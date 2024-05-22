6d438632c2e929bedef92f28bd253a2d6152e32b
luiz ilha <luizilham@gmail.com>
2024-05-22 15:29:03 -0300

    main func set people router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,29 +1,13 @@
 package main
 
 import (
-//	"errors"
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"luizilha/rinha-ilha/internal/people"
 )
-
-type pessoa struct {
-	ID         string `json:"id"`
-	apelido    string
-	nome       string
-	nascimento string
-	stack      []string
-}
-
-var pessoas = []pessoa{
-  { ID: "nsestrtsne", apelido: "ilha", nome: "luiz", nascimento: "07/03/1993", stack: []string{} },
-}
-
-func contagemPessoas(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, len(pessoas))
-}
 
 func main() {
 	router := gin.Default()
-	router.GET("/contagem-pessoas", contagemPessoas)
+	peopleRouter := people.NewPeopleRouter(router)
+	peopleRouter.Registry()
 	router.Run(":8080")
 }
