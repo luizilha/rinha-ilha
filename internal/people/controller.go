0c4fac3c8af101df5e6a332e359f223e9eae9cd3
luiz ilha <luizilham@gmail.com>
2024-06-01 23:42:58 -0300

    findbyId Controller

--- internal/people/controller.go
+++ internal/people/controller.go
@@ -8,44 +8,49 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type controller struct { }
+type Controller struct{}
 
-/*
-func (con *controller) AllPeople(c *gin.Context) {
-  db := storage.PostgresConnection()
-  repo := NewRepository(db)
-	all, err := repo.AllPeople()
+func (con *Controller) Count(c *gin.Context) {
+	db := storage.PostgresConnection()
+	repo := NewRepository(db)
+	value, err := repo.Count()
 	if err != nil {
-    c.IndentedJSON(http.StatusBadRequest, err.Error())
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
 	}
-	c.IndentedJSON(http.StatusOK, all)
-}
-*/
-
-func (con *controller) Count(c *gin.Context) {
-  db := storage.PostgresConnection()
-  repo := NewRepository(db)
-  value, err := repo.Count()
-  if err != nil {
-    c.IndentedJSON(http.StatusBadRequest, err.Error())
-  }
-  c.IndentedJSON(http.StatusOK, value)
+	c.IndentedJSON(http.StatusOK, value)
 
 }
 
-func (con *controller) Insert(c *gin.Context) {
-  db := storage.PostgresConnection()
-  repo := NewRepository(db)
+func (con *Controller) Insert(c *gin.Context) {
+	db := storage.PostgresConnection()
+	repo := NewRepository(db)
 
-  var newPeople types.People
-  if err0 := c.BindJSON(&newPeople); err0 != nil {
-    c.IndentedJSON(http.StatusBadRequest, err0)
-  }
-  
-  row, err := repo.Insert(&newPeople)
-  if err != nil {
-    c.IndentedJSON(http.StatusBadRequest, err.Error())
-  } else {
-    c.IndentedJSON(http.StatusCreated, row)
-  }
+	var newPeople types.People
+	if err0 := c.BindJSON(&newPeople); err0 != nil {
+		c.IndentedJSON(http.StatusBadRequest, err0)
+	}
+
+	row, err := repo.Insert(&newPeople)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+	} else {
+		c.IndentedJSON(http.StatusCreated, row)
+	}
+}
+
+func (con *Controller) FindById(c *gin.Context) {
+	db := storage.PostgresConnection()
+	repo := NewRepository(db)
+
+	var uri types.UriID
+	if err := c.ShouldBindUri(&uri); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	p, err0 := repo.FindById(uri.ID)
+	if err0 != nil {
+		c.IndentedJSON(http.StatusBadRequest, err0.Error())
+	} else {
+		c.IndentedJSON(http.StatusOK, p)
+	}
 }
